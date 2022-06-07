f6de930dfbb2ad1005548465a38e172b161497b9
spapa013 <stelios@spapa.us>
2022-06-07 04:42:44 -0500

    update setup.py to include package data and add manifest.in

--- python/microns-coregistration-api/setup.py
+++ python/microns-coregistration-api/setup.py
@@ -16,5 +16,7 @@ setup(
     description="api for microns-coregistration",
     author="Stelios Papadopoulos, Christos Papadopoulos",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['transform/data/*.npy']},
     install_requires=requirements
 )
