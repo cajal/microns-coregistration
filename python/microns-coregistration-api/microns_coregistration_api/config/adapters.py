6ecd63a0f17bf12baffa7466eb263f3102fe0049
spapa013 <stelios@spapa.us>
2022-07-26 04:40:10 -0500

    adds dashboard and cell_type_trainer to config

--- python/microns-coregistration-api/microns_coregistration_api/config/adapters.py
+++ python/microns-coregistration-api/microns_coregistration_api/config/adapters.py
@@ -2,14 +2,17 @@
 Adapters for DataJoint tables.
 """
 
-from microns_utils.adapter_utils import NumpyAdapter
+from microns_utils.adapter_utils import NumpyAdapter, JsonAdapter
 
 minnie_stacks = NumpyAdapter('filepath@minnie_stacks')
 multimatch_stacks = NumpyAdapter('filepath@multimatch_stacks')
+dashboard = JsonAdapter('filepath@dashboard')
+cell_type_trainer = JsonAdapter('filepath@cell_type_trainer')
 
 minnie_em = {'minnie_stacks': minnie_stacks}
 minnie65_auto_match = {}
 minnie65_coregistration = {}
 minnie65_manual_match = {}
 multimatch = {'multimatch_stacks': multimatch_stacks}
-dashboard = {}
+dashboard = {'dashboard': dashboard}
+cell_type_trainer = {'cell_type_trainer': cell_type_trainer}
--- python/microns-coregistration-api/microns_coregistration_api/config/externals.py
+++ python/microns-coregistration-api/microns_coregistration_api/config/externals.py
@@ -9,6 +9,8 @@ import datajoint_plus as djp
 base_path = Path() / '/mnt' / 'dj-stor01' / 'microns'
 minnie_stack_path = base_path / 'minnie' / 'stacks'
 multimatch_stack_path = Path() / '/mnt' / 'scratch09' / 'microns' / 'stacks'
+dashboard_path = base_path / 'minnie' / 'dashboard'
+cell_type_trainer_path = dashboard_path / 'cell_type_trainer'
 
 minnie_em = {
     'minnie_stacks': djp.make_store_dict(minnie_stack_path)
@@ -30,4 +32,10 @@ multimatch = {
     'multimatch_stacks': djp.make_store_dict(multimatch_stack_path)
 }
 
-dashboard = {}
+dashboard = {
+    'dashboard': djp.make_store_dict(dashboard_path)
+}
+
+cell_type_trainer = {
+    'cell_type_trainer': djp.make_store_dict(cell_type_trainer_path)
+}
