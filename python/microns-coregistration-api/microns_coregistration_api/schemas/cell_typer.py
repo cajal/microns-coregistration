10129a7956afd3094cd76f9f674170f5bc80281e
spapa013 <stelios@spapa.us>
2022-08-29 13:38:29 -0500

    rewrite cell_typer schema

--- python/microns-coregistration-api/microns_coregistration_api/schemas/cell_typer.py
+++ python/microns-coregistration-api/microns_coregistration_api/schemas/cell_typer.py
@@ -2,8 +2,7 @@
 DataJoint tables for cell typing.
 """
 
-import json
-import traceback
+from imp import reload
 import pandas as pd
 from pathlib import Path
 import datajoint_plus as djp
@@ -19,7 +18,7 @@ from microns_morphology_api.schemas import \
 
 from microns_dashboard_api.schemas import \
     dashboard as db
-import microns_dashboard_api as mdb_api
+import microns_dashboard_api as mdb
 
 from ..config import cell_typer_config as config
 
@@ -28,38 +27,66 @@ config.register_adapters(context=locals())
 
 schema = djp.schema(config.schema_name, create_schema=True)
 
-version_attr = """-> Version \n"""
-user_attr = """user : varchar(128) # dashboard username \n"""
-
+@schema
+class Tag(mdb.VersionLookup):
+    package = 'microns-coregistration-api'
+    attr_name = 'tag'
 
 @schema
-class Version(mdb_api.VersionLookup):
-    package = 'microns-coregistration-api'
+class User(djp.Lookup):
+    definition = f"""
+    user : varchar(128) # dashboard username
+    ---
+    timestamp=CURRENT_TIMESTAMP : timestamp
+    """
 
+    class MICrONSDashboard(djp.Part, dj.Lookup):
+        definition = f"""
+        -> master
+        -> db.User
+        ---
+        {db.User.hash_name} : varchar({db.User.hash_len}) # {db.User.comment}
+        """
+        
+        @classproperty
+        def contents(cls):
+            cls.insert(db.User.Add.proj(), ignore_extra_fields=True, skip_duplicates=True, insert_to_master=True)
+            return {}
 
 @schema
 class Protocol(djp.Lookup):
     hash_name = 'protocol_id'
     definition = """
     protocol_id : varchar(8)
+    ---
+    timestamp=CURRENT_TIMESTAMP : timestamp # 
     """
 
     @classmethod
     def get(cls, key, n=None, subtract=None):
         return cls.r1p(key, include_parts=cls.Info).get(key, n=n, subtract=subtract)
 
+    class StaticSource(djp.Part):
+        definition = """
+        -> master
+        idx : int unsigned
+        -> m65mat.Segment
+        ---
+        cell_type=NULL : varchar(256)
+        other_info=NULL : longblob
+        """
+    
     class Info(djp.Part, dj.Lookup):
         enable_hashing = True
         hash_name = 'protocol_id'
-        hashed_attrs = 'protocol_name', 'version_id'
+        hashed_attrs = 'protocol_name', Tag.attr_name
         definition = f"""
         -> master
         protocol_name : varchar(128)
+        -> {Tag.class_name}
         ---
-        -> Version
         source_type=NULL : varchar(128)
         description=NULL : longblob
-        ts_inserted=CURRENT_TIMESTAMP : timestamp
         """
 
         @property
@@ -67,7 +94,7 @@ class Protocol(djp.Lookup):
             contents = [
                 {
                     'protocol_name': 'training', 
-                    'version_id': Version.current_version_id, 
+                    Tag.attr_name: Tag.version, 
                     'source_type': 'dynamic',
                     'description': 'minnie65_materialization.Segment.Nucleus \
                         * minnie65_auto_proofreading.AutoProofreadNeuron & multiplicity=1 \
@@ -77,32 +104,37 @@ class Protocol(djp.Lookup):
             ] + [
                 { 
                     'protocol_name': f'validation_{n}', 
-                    'version_id': Version.current_version_id, 
+                    Tag.attr_name: Tag.version, 
                     'source_type': 'static',
-                    'description': f'{n} random segments from training protocol with materialization version 343'} for n in [50, 20, 10]
+                    'description': f'{n} random segments from training protocol with materialization version 343'} for n in [10, 20, 50]
             ] + [
                 { 
                     'protocol_name': f'evaluation_{n}', 
-                    'version_id': Version.current_version_id, 
+                    Tag.attr_name: Tag.version, 
                     'source_type': 'static',
-                    'description': f'{n} random segments from training protocol with materialization version 343'} for n in [50, 20, 10]
+                    'description': f'{n} random segments from training protocol with materialization version 343'} for n in [10, 20, 50]
             ]
             cls.insert(contents, insert_to_master=True, skip_duplicates=True, ignore_extra_fields=True)
+            protocol_id_source = djp.U('protocol_id') & (cls & {'source_type="static"'})
+            static_source = djp.U('protocol_id') & cls.master.StaticSource()
+            new_protocols = protocol_id_source - static_source
+            if len(new_protocols) > 0:
+                cls.master.Log('info', 'Found new static protocols. Adding...')
+                cls.load_dependencies()
+                cls.fill_static_protocols(new_protocols)
             return {}
 
         
         @classmethod
-        def fill_static_protocols(cls):
-            for key in cls:
+        def fill_static_protocols(cls, protocols):
+            for key in protocols:
                 protocol_id = key.get('protocol_id')
-                protocol_name = key.get('protocol_name')
-                source_type = key.get('source_type')
-                if source_type == 'static':
-                    if 'validation' in protocol_name or 'evaluation' in protocol_name:
-                        n = int(protocol_name.split('_')[1])
-                        df = cls.master.get({'protocol_name': 'training', 'version_id': Version.current_version_id, 'ver': 343}, n=n).reset_index().rename(columns={'index': 'idx'})
-                        df['protocol_id'] = protocol_id
-                        cls.master.StaticSource.insert(df, ignore_extra_fields=True, skip_duplicates=True)
+                protocol_name = (cls & key).fetch1('protocol_name')
+                if 'validation' in protocol_name or 'evaluation' in protocol_name:
+                    n = int(protocol_name.split('_')[1])
+                    df = cls.master.get({'protocol_name': 'training', Tag.attr_name: Tag.version, 'ver': 343}, n=n).reset_index().rename(columns={'index': 'idx'})
+                    df['protocol_id'] = protocol_id
+                    cls.master.StaticSource.insert(df, ignore_extra_fields=True, skip_duplicates=True)
 
         def to_df(self, rel, n=None):
             if n is None:
@@ -112,7 +144,7 @@ class Protocol(djp.Lookup):
 
         def get(self, key, n=None, subtract=None):
             params = self.fetch1()
-            assert params.get('version_id') == Version.current_version_id, 'Version mismatch'
+            assert params.get(Tag.attr_name) == Tag.version, 'Version mismatch'
             protocol_id = params.get('protocol_id')
             protocol_name = params.get('protocol_name')
             source_type = params.get('source_type')
@@ -126,24 +158,14 @@ class Protocol(djp.Lookup):
                     autoproof_rel = (m65auto.AutoProofreadNeuron & 'multiplicity=1' & 'baylor_cell_type_after_proof=external_cell_type' & 'baylor_cell_type_exc_probability_after_proof > 0.95').proj(cell_type="baylor_cell_type_after_proof")
                     rel = djp.U('segment_id', 'nucleus_id', 'cell_type') & (segment_rel * autoproof_rel)
                     unique_segs = djp.U('segment_id').aggr(rel, n_seg='count(segment_id)') & 'n_seg=1'
-                    evaluation_segs = self.master.StaticSource & (self.__class__ & 'protocol_name LIKE "evaluation%"')
-                    final = rel & unique_segs.proj() - evaluation_segs.proj()
+                    sequestered_segs = self.master.StaticSource & (self.__class__ & ['protocol_name LIKE "validation%"', 'protocol_name LIKE "evaluation%"'])
+                    final = rel & unique_segs.proj() - sequestered_segs.proj()
                     return self.to_df((final - subtract) & key, n=n)
                 else:
                     raise AttributeError(f'protocol_name {protocol_name} not found.')
             else:
                 raise AttributeError(f'source_type {source_type} not found.')
 
-    class StaticSource(djp.Part):
-        definition = """
-        -> master
-        idx : int unsigned
-        -> m65mat.Segment
-        ---
-        cell_type=NULL : varchar(256)
-        other_info=NULL : longblob
-        """
-    
     class Manager(djp.Part, dj.Lookup):  
         hash_name = 'protocol_id'
         definition = """
@@ -156,44 +178,61 @@ class Protocol(djp.Lookup):
         def contents(cls):
             cls.insert(cls.master.Info, ignore_extra_fields=True, skip_duplicates=True)
             return {}
+            
 @schema
-class Event(mdb_api.EventLookup):
+class Event(mdb.EventLookup):
     basedir = Path(config.externals.get('cell_typer_files').get('location'))
-    additional_secondary_attrs = version_attr
+    extra_secondary_attrs = f"""
+    -> {Tag.class_name}
+    """
 
-    class Submission(mdb_api.EventType):
-        event_types = 'cell_type_submission', 'flagged_submission'
-        constant_attrs = {'version_id': Version.current_version_id}
-        additional_primary_attrs = version_attr
-        additional_secondary_attrs = user_attr
+    class Submission(mdb.Event):
+        events = 'cell_type_submission', 'flagged_submission'
+        constant_attrs = {Tag.attr_name: Tag.version}
+        extra_primary_attrs = f"""
+        -> {Tag.class_name}
+        """
+        extra_secondary_attrs = f"""
+        -> {User.class_name}
+        """
 
         def on_event(self, event):
-            Submission.Maker.populate({'event_id': event.id})
+            Submission.Add.populate({'event_id': event.id})
 
 @schema
-class EventHandler(mdb_api.EventHandlerLookup):
-    current_version_id = Version.current_version_id
+class EventHandler(mdb.EventHandlerLookup):
+    @classmethod
+    def run(cls, key):
+        handler = cls.r1p(key)
+        handler_event = handler.fetch1('event')
+        handler_version = handler.fetch1(Tag.attr_name)
+        cls.Log('info',  'Running %s', handler.class_name)
+        cls.Log('debug', 'Running %s with key %s', handler.class_name, key)
+        assert handler_event == key['event'], f'event in handler {handler_event} doesnt match event in key {key["event"]}'
+        assert handler_version == Tag.version, f'version mismatch, event_handler version_id is {handler_version} but the current version_id is {Tag.version}'
+        key = handler.run(key)
+        cls.Log('info', '%s ran successfully.', handler.class_name)
+        return key
 
-    class Submission(djp.Part, dj.Lookup):
-        enable_hashing = True
-        hash_name = 'event_handler_id'
-        hashed_attrs = 'version_id', 'event_type'
-        definition = """
-        -> master
-        event_type : varchar(450) # type of event that method handles
-        -> Version
+    class Submission(mdb.EventHandler):
+        hashed_attrs = 'event', Tag.attr_name
+        constant_attrs = {Tag.attr_name: Tag.version}
+        extra_primary_attrs = f"""
+        -> {Tag.class_name}
         """
 
         @classproperty
         def contents(cls):
-            for event_type in Event.Submission.event_types:
-                cls.insert({'event_type': event_type, 'version_id': Version.current_version_id}, ignore_extra_fields=True, skip_duplicates=True, insert_to_master=True)
+            for event in Event.Submission.events:
+                key = {'event': event}
+                key.update(cls.constant_attrs)
+                cls.insert(key, ignore_extra_fields=True, skip_duplicates=True, insert_to_master=True)
             return {}
 
         def run(self, key):
-            event_type = self.fetch1('event_type')
+            event = self.fetch1('event')
             data = key.get('data')
-            if event_type in ['cell_type_submission']:
+            if event in ['cell_type_submission']:
                 required_attrs = ['protocol_id', 'segment_id', 'user_choice']
                 assert data is not None, f'data must include: {required_attrs}'
                 data.setdefault('cell_type_label', None)
@@ -206,7 +245,7 @@ class EventHandler(mdb_api.EventHandlerLookup):
                 key['other_data'] = data if data else None
                 return key
             
-            if event_type in ['flagged_submission']:
+            if event in ['flagged_submission']:
                 required_attrs = ['protocol_id', 'segment_id', 'note']
                 assert data is not None, f'data must include: {required_attrs}'
                 key['protocol_id'] = data.pop('protocol_id')
@@ -221,53 +260,28 @@ class Submission(djp.Lookup):
     hash_name = 'submission_id'
     definition = """
     submission_id : varchar(10)
+    ---
+    timestamp=CURRENT_TIMESTAMP : timestamp # 
     """
 
-    class Maker(djp.Part, dj.Computed):
-        enable_hashing=True
+    class Add(mdb.Maker):
         hash_name = 'submission_id'
-        hashed_attrs = Event.primary_key + EventHandler.primary_key
-        definition = """
-        -> master
-        -> Event
-        -> EventHandler
-        ---
-        ts_inserted_maker=CURRENT_TIMESTAMP : timestamp # timestamp inserted to Maker
-        """
-
+        upstream = Event
+        method = EventHandler
         @classproperty
         def key_source(cls):
-            return (djp.U('event_id', 'event_handler_id') & (Event.Submission * EventHandler.Submission)) - cls.master.Attempted
+            return (djp.U('event_id', 'event_handler_id') & (Event.Submission * EventHandler.Submission))
 
-        def make(self, key):
-            key[self.hash_name] = self.hash1(key)
-            try:
-                key.update(Event.get1(key))
-                key.update(EventHandler.run(key))
-                self.insert1(key, ignore_extra_fields=True, insert_to_master=True, insert_to_master_kws={'ignore_extra_fields': True, 'skip_duplicates': True}, skip_hashing=True)
-                
-                if key.get('event_type') in ['cell_type_submission']:
-                    self.master.CellType.insert1(key, ignore_extra_fields=True)
-                elif key.get('event_type') in ['flagged_submission']:
-                    self.master.Flagged.insert1(key, ignore_extra_fields=True)
-            except:
-                key['traceback'] = traceback.format_exc()
-                self.master.Attempted.insert1(key, insert_to_master=True, ignore_extra_fields=True)
-                self.master.Log('exception', 'Error inserting Submission')
-    
-    class Attempted(djp.Part):
-        definition = """
-        -> master
-        -> Event
-        -> EventHandler
-        ---
-        traceback=NULL : longblob
-        ts_inserted_attempted=CURRENT_TIMESTAMP : timestamp # timestamp inserted into Attempted
-        """
+        def on_make(self, key):
+            if key.get('event') in ['cell_type_submission']:
+                self.master.CellType.insert1(key, ignore_extra_fields=True)
+
+            elif key.get('event') in ['flagged_submission']:
+                self.master.Flagged.insert1(key, ignore_extra_fields=True)
 
     class CellType(djp.Part):
-        definition = """
-        -> db.UserInfo
+        definition = f"""
+        -> User
         -> Protocol
         -> m65mat.Segment
         -> master
@@ -276,19 +290,17 @@ class Submission(djp.Lookup):
         cell_type_label=NULL : varchar(128) # cell type label (optional)
         note=NULL : longblob # user note (optional)
         other_data=NULL : longblob # other data (optional)
-        ts_inserted_store=CURRENT_TIMESTAMP : timestamp # timestamp inserted into store
         """
     
     class Flagged(djp.Part):
-        definition = """
-        -> db.UserInfo
+        definition = f"""
+        -> User
         -> Protocol
         -> m65mat.Segment
         -> master
         ---
         note=NULL : longblob # user note (optional)
         other_data=NULL : longblob # other data (optional)
-        ts_inserted_store=CURRENT_TIMESTAMP : timestamp # timestamp inserted into store
         """
 
 schema.spawn_missing_classes()
